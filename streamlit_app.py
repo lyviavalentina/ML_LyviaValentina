fba671065e6122a0230d3305153190aaed566d3a
lyviavalentina <143624275+lyviavalentina@users.noreply.github.com>
2024-12-26 21:44:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title('👩🏻‍🎓 Student Performance Factors Machine Learning')
 
-st.write('Hello world!')
+st.write('This is app builds a machine learning model!')
