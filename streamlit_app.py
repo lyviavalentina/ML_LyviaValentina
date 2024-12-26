18333f1ce8184e71a4c629f68dbd86467085b079
lyviavalentina <143624275+lyviavalentina@users.noreply.github.com>
2024-12-26 23:13:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import seaborn as sns
-import matplotlib.pyplot as plt
+import plotly as plt
 
 st.title('👩🏻‍🎓 Student Performance Factors Machine Learning')
 
