0428168aa4d34143681cf4db84ec01d372b58994
lyviavalentina <143624275+lyviavalentina@users.noreply.github.com>
2024-12-26 22:49:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 st.title('👩🏻‍🎓 Student Performance Factors Machine Learning')
 
@@ -9,3 +12,4 @@ with st.expander('Data'):
   st.write('**Raw Data**')
   df = pd.read_csv('https://raw.githubusercontent.com/lyviavalentina/data/refs/heads/main/StudentPerformanceFactors%20(1).csv')
   df
+
