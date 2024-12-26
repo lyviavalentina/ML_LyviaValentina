8a90b35954d6d109c2f647b59028336c76db6a7e
lyviavalentina <143624275+lyviavalentina@users.noreply.github.com>
2024-12-26 22:46:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,7 @@ st.title('👩🏻‍🎓 Student Performance Factors Machine Learning')
 
 st.write('This is app builds a machine learning model!')
 
+with st.expander('Data')
+st.write('**Raw Data**')
 df = pd.read_csv('https://raw.githubusercontent.com/lyviavalentina/data/refs/heads/main/StudentPerformanceFactors%20(1).csv')
+df
