c253065431d7c07745a6f74b9f218cccb56c35b7
lyviavalentina <143624275+lyviavalentina@users.noreply.github.com>
2024-12-26 22:51:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,25 @@ st.write('This is app builds a machine learning model!')
 with st.expander('Data'):
   st.write('**Raw Data**')
   df = pd.read_csv('https://raw.githubusercontent.com/lyviavalentina/data/refs/heads/main/StudentPerformanceFactors%20(1).csv')
-  df
 
+# Replace categorical values in 'Teacher_Quality', 'Parental_Education_Level', 'Distance_from_Home' with numerical values
+df['Teacher_Quality'] = df['Teacher_Quality'].replace({'Low': 0, 'Medium': 1, 'High': 2})
+df['Parental_Education_Level'] = df['Parental_Education_Level'].replace({'High School': 0, 'College': 1, 'Postgraduate': 2})
+df['Distance_from_Home'] = df['Distance_from_Home'].replace({'Near': 0, 'Moderate': 1, 'Far': 2})
+
+mean1 = df['Teacher_Quality'].mean()
+df['Teacher_Quality'] = df['Teacher_Quality'].fillna(mean1)
+
+mean2 = df['Parental_Education_Level'].mean()
+df['Parental_Education_Level'] = df['Parental_Education_Level'].fillna(mean2)
+
+mean3 = df['Distance_from_Home'].mean()
+df['Distance_from_Home'] = df['Distance_from_Home'].fillna(mean3)
+
+df.isnull().any()
+
+df['Teacher_Quality'] = df['Teacher_Quality'].replace({0: 'Low', 1: 'Medium', 2: 'High'})
+df['Parental_Education_Level'] = df['Parental_Education_Level'].replace({0: 'High School', 1: 'College', 2: 'Postgraduate'})
+df['Distance_from_Home'] = df['Distance_from_Home'].replace({0: 'Near', 1: 'Moderate', 2: 'Far'})
+
+df
